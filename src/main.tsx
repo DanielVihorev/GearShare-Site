fd68cb61021e93fb3037dc24079d5f69fc384d66
Kirill <kirill.novikov@gmail.com>
2025-08-20 19:20:49 +0300

    Fix environment variable configuration for Vite and update Clarity initialization 2

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import "./index.css";
 import AuthProvider from "./context/AuthContext.tsx";
 
 // Initialize Microsoft Clarity
-import clarity from "@microsoft/clarity";
+import Clarity from "@microsoft/clarity";
 
 // Initialize Clarity
 const clarityID = import.meta.env.VITE_CLARITY_PROJECT_ID;
 
 if (clarityID && import.meta.env.PROD) {
-  clarity.init(clarityID);
+  Clarity.init(clarityID);
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
